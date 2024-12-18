fd1893a8fde1de85869eb901e448fe611a4f600f
gidBell <gideonogirima25@gmail.com>
2024-12-18 11:13:29 +0100

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,6 @@ export default function Main(props) {
     return (
         <div className="imgContainer">
             <img src={data.hdurl} alt={data.title || 'bg-img'} className="bgImage" />
-
         </div>
 
     )
